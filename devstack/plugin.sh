17089520849be7e38f948c6e7717d3e49f6e9f25
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2016-09-21 12:15:23 +0800

    Update plugin.sh

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,51 +1,64 @@
 # plugin.sh - DevStack plugin.sh dispatch script template
 
-function install_template {
-    cp -r $HORIZON_DIR/openstack_dashboard/themes/default $HORIZON_DIR/openstack_dashboard/themes/mytheme
-}
-
-function init_template {
+function prepare_mytheme {
     :
 }
 
-function configure_template {
+function install_mytheme {
+    cp -r $HORIZON_DIR/openstack_dashboard/themes/default $HORIZON_DIR/openstack_dashboard/themes/mytheme
+}
+
+function configure_mytheme {
     local local_settings=$HORIZON_DIR/openstack_dashboard/local/local_settings.py
-    _horizon_config_set $local_settings "" AVAILABLE_THEMES \[\(\'default\',\'Default\',\'themes/default\'\),\]
+    _horizon_config_set $local_settings "" AVAILABLE_THEMES \[\(\'default\',\'Default\',\'themes/mytheme\'\),\]
+}
+
+function init_mytheme {
+    :
+}
+
+function unstack_mytheme {
+    rm -rf $HORIZON_DIR/openstack_dashboard/themes/mytheme
+}
+
+function clean_mytheme {
+    :
 }
 
 # check for service enabled
-if is_service_enabled horizon template; then
+if is_service_enabled horizon mytheme; then
 
     if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
         # Set up system services
-        echo_summary "Configuring system services for Template"
-        # install_package cowsay
+        echo_summary "Configuring system services for MyTheme"
+        prepare_mytheme
 
     elif [[ "$1" == "stack" && "$2" == "install" ]]; then
         # Perform installation of service source
-        echo_summary "Installing Template"
-        install_template
+        echo_summary "Installing MyTheme"
+        install_mytheme
 
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         # Configure after the other layer 1 and 2 services have been configured
-        echo_summary "Configuring Template"
-        configure_template
+        echo_summary "Configuring MyTheme"
+        configure_mytheme
 
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         # Initialize and start the template service
-        echo_summary "Initializing Template"
-        init_template
+        echo_summary "Initializing MyTheme"
+        init_mytheme
     fi
 
     if [[ "$1" == "unstack" ]]; then
         # Shut down template services
-        echo_summary "Unstacking Template"
-        rm -rf $HORIZON_DIR/openstack_dashboard/themes/mytheme
+        echo_summary "Unstacking MyTheme"
+        unstack_mytheme
     fi
 
     if [[ "$1" == "clean" ]]; then
         # Remove state and transient data
         # Remember clean.sh first calls unstack.sh
-        echo_summary "Cleaning Template"
+        echo_summary "Cleaning MyTheme"
+        clean_mytheme
     fi
 fi
