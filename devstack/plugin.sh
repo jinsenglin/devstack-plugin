538f8ca72591d85dd94a7d7eb85d614a7a956492
Chun Cheng Lin <jinsenglin@users.noreply.github.com>
2016-09-21 14:56:20 +0800

    Update plugin.sh

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -1,82 +1,64 @@
 # plugin.sh - DevStack plugin.sh dispatch script template
 
-function prepare_mytheme {
+function prepare_mypanel {
     :
 }
 
-function install_mytheme {
-    cp -r $HORIZON_DIR/openstack_dashboard/themes/default $HORIZON_DIR/openstack_dashboard/themes/mytheme
-    mkdir $HORIZON_DIR/openstack_dashboard/themes/mytheme/img
-    
-    if [ $MYTHEME_ICO == "NOTHING" ]; then
-        touch $HORIZON_DIR/openstack_dashboard/themes/mytheme/img/favicon.ico
-    else
-        wget $MYTHEME_ICO -O $HORIZON_DIR/openstack_dashboard/themes/mytheme/img/favicon.ico
-    fi
-    if [ $MYTHEME_LOGO == "NOTHING" ]; then
-        touch $HORIZON_DIR/openstack_dashboard/themes/mytheme/img/logo.png
-    else
-        wget $MYTHEME_LOGO -O $HORIZON_DIR/openstack_dashboard/themes/mytheme/img/logo.png
-    fi
-    if [ $MYTHEME_LOGO_SPLASH == "NOTHING" ]; then
-        touch $HORIZON_DIR/openstack_dashboard/themes/mytheme/img/logo-splash.png
-    else
-        wget $MYTHEME_LOGO_SPLASH -O $HORIZON_DIR/openstack_dashboard/themes/mytheme/img/logo-splash.png
-    fi
+function install_mypanel {
+    git_clone $MYPANEL_REPO $MYPANEL_REPO_DIR_NAME $MYPANEL_REPO_BRANCH
 }
 
-function configure_mytheme {
-    local local_settings=$HORIZON_DIR/openstack_dashboard/local/local_settings.py
-    _horizon_config_set $local_settings "" AVAILABLE_THEMES \[\(\'default\',\'Default\',\'themes/mytheme\'\),\]
-    _horizon_config_set $local_settings "" SITE_BRANDING "'My OpenStack Dashboard'"
+function configure_mypanel {
+    setup_develop $MYPANEL_REPO_DIR_NAME
+    cp $MYPANEL_REPO_DIR_NAME/mypanel_ui/enabled/$MYPANEL_ENABLED_FILE $HORIZON_DIR/openstack_dashboard/local/enabled
 }
 
-function init_mytheme {
+function init_mypanel {
     :
 }
 
-function unstack_mytheme {
-    rm -rf $HORIZON_DIR/openstack_dashboard/themes/mytheme
+function unstack_mypanel {
+    rm -f $HORIZON_DIR/openstack_dashboard/local/enabled/$MYPANEL_ENABLED_FILE
 }
 
-function clean_mytheme {
+function clean_mypanel {
     :
 }
 
 # check for service enabled
-if is_service_enabled horizon mytheme; then
+if is_service_enabled horizon mypanel; then
 
     if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
         # Set up system services
-        echo_summary "Configuring system services for MyTheme"
-        prepare_mytheme
+        echo_summary "Configuring system services for MyPanel"
+        prepare_mypanel
 
     elif [[ "$1" == "stack" && "$2" == "install" ]]; then
         # Perform installation of service source
-        echo_summary "Installing MyTheme"
-        install_mytheme
+        echo_summary "Installing MyPanel"
+        install_mypanel
 
     elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
         # Configure after the other layer 1 and 2 services have been configured
-        echo_summary "Configuring MyTheme"
-        configure_mytheme
+        echo_summary "Configuring MyPanel"
+        configure_mypanel
 
     elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
         # Initialize and start the template service
-        echo_summary "Initializing MyTheme"
-        init_mytheme
+        echo_summary "Initializing MyPanel"
+        init_mypanel
     fi
 
     if [[ "$1" == "unstack" ]]; then
         # Shut down template services
-        echo_summary "Unstacking MyTheme"
-        unstack_mytheme
+        echo_summary "Unstacking MyPanel"
+        unstack_mypanel
     fi
 
     if [[ "$1" == "clean" ]]; then
         # Remove state and transient data
         # Remember clean.sh first calls unstack.sh
-        echo_summary "Cleaning MyTheme"
-        clean_mytheme
+        echo_summary "Cleaning MyPanel"
+        clean_mypanel
     fi
 fi
